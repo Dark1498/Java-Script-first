f6161fcb7df6d45b5e2bfbcc7f236ac09fe6b9e3
DarkKnight <aza1498@mail.ru>
2020-01-31 11:42:21 +0600

    Change files

--- es6/test.js
+++ es6/test.js
@@ -2,7 +2,7 @@
 // средство еs б: блок "левых" объявлений
 const sentences = [
 {subject: 'JavaScript', verb: 'is', object : 'great' },
-{subject: 'Elephants', verb: 'are', object : 'large' },
+{subject: 'River', verb: 'are', object : 'large' },
 ];
 // средство еsб: деструктуризация объекта
 function say({ subject, verb, object }) {
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
 // Зависимости gulp
 gulp.task('default',function(){
+  //CODE eslint
+  gulp.src(["es6/**/*.js", "public/es6/**/*.js"])
+      .pipe(eslint())
+      .pipe(eslint.format());
   //CODE NODE
-
   gulp.src("es6/**/*.js")
       .pipe(babel())
       .pipe(gulp.dest("dist"));
